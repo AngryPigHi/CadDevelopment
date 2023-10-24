b8025a8eee5cd8f602d089f0b04cec80fe73df63
Blithe Zhu <2375512561@qq.com>
2023-10-24 16:48:32 +0800

    20231023

---
+++ C用户交互/SeeEntityExam.cs
@@ -0,0 +1,151 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using CADTools;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C用户交互
+{
+    public class SeeEntityExam
+    {
+
+        [CommandMethod("SeePolyline")]
+        public void SeePolyline()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            Database database = HostApplicationServices.WorkingDatabase;
+
+            //1.获取输入的字符串
+            PromptStringOptions psOption = new PromptStringOptions("\n 请输入点集字符串：");
+            psOption.AllowSpaces = true;
+
+            PromptResult pResult = editor.GetString(psOption);
+            if (pResult.Status == PromptStatus.OK)
+            {
+                //2.将字符串转为点集
+                string pointsString = pResult.StringResult;
+                if (!string.IsNullOrWhiteSpace(pointsString))
+                {
+                    List<Point2dCanSet> point2Ds = JsonConvert.DeserializeObject<List<Point2dCanSet>>(pointsString);
+                    Polyline polyline = new Polyline();
+                    for (int i = 0; i < point2Ds.Count; i++)
+                    {
+                        Point2d pointNew = new Point2d(point2Ds[i].X, point2Ds[i].Y);
+                        polyline.AddVertexAt(i, pointNew, 0, 0, 0);
+                    }
+                    polyline.Closed = true;
+
+                    database.AddEntityToModelSpace(polyline);
+                }
+            }
+
+
+        }
+
+        [CommandMethod("SeeListPolyline")]
+        public void SeeListPolyline()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            Database database = HostApplicationServices.WorkingDatabase;
+
+            //1.获取输入的字符串
+            PromptStringOptions psOption = new PromptStringOptions("\n 请输入多点集字符串：");
+            psOption.AllowSpaces = true;
+
+            PromptResult pResult = editor.GetString(psOption);
+            if (pResult.Status == PromptStatus.OK)
+            {
+                //2.将字符串转为点集
+                string pointsString = pResult.StringResult;
+                if (!string.IsNullOrWhiteSpace(pointsString))
+                {
+                    List<List<Point2dCanSet>> polylines = JsonConvert.DeserializeObject<List<List<Point2dCanSet>>>(pointsString);
+
+                    List<Polyline> resultPolylines = new List<Polyline>();
+
+                    foreach (var point2Ds in polylines)
+                    {
+                        Polyline singlePolyline = new Polyline();
+                        for (int i = 0; i < point2Ds.Count; i++)
+                        {
+                            Point2d pointNew = new Point2d(point2Ds[i].X, point2Ds[i].Y);
+                            singlePolyline.AddVertexAt(i, pointNew, 0, 0, 0);
+                        }
+                        singlePolyline.Closed = true;
+                        resultPolylines.Add(singlePolyline);
+                    }
+
+                    foreach (var item in resultPolylines)
+                    {
+                        database.AddEntityToModelSpace(item);
+                    }
+
+                }
+            }
+        }
+
+
+        [CommandMethod("SeeListPolylineFromTxt")]
+        public void SeeListPolylineFromTxt()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+            Database database = HostApplicationServices.WorkingDatabase;
+
+            //1.获取输入的字符串
+            PromptStringOptions psOption = new PromptStringOptions("\n 请输入文档路径：");
+            psOption.AllowSpaces = true;
+
+            PromptResult pResult = editor.GetString(psOption);
+            if (pResult.Status == PromptStatus.OK)
+            {
+                //2.将字符串转为点集
+                string filePath = pResult.StringResult;
+
+                string pointsString = File.ReadAllText(filePath);
+
+                if (!string.IsNullOrWhiteSpace(pointsString))
+                {
+                    List<List<Point2dCanSet>> polylines = JsonConvert.DeserializeObject<List<List<Point2dCanSet>>>(pointsString);
+
+                    List<Polyline> resultPolylines = new List<Polyline>();
+
+                    foreach (var point2Ds in polylines)
+                    {
+                        Polyline singlePolyline = new Polyline();
+                        for (int i = 0; i < point2Ds.Count; i++)
+                        {
+                            Point2d pointNew = new Point2d(point2Ds[i].X, point2Ds[i].Y);
+                            singlePolyline.AddVertexAt(i, pointNew, 0, 0, 0);
+                        }
+                        singlePolyline.Closed = true;
+                        resultPolylines.Add(singlePolyline);
+                    }
+
+                    foreach (var item in resultPolylines)
+                    {
+                        database.AddEntityToModelSpace(item);
+                    }
+
+                }
+            }
+        }
+
+
+
+
+    }
+
+    public class Point2dCanSet
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ C用户交互/SelectExam.cs
@@ -0,0 +1,69 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C用户交互
+{
+    public class SelectExam
+    {
+
+        [CommandMethod("SelectDemo")]
+        public void SelectDemo()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            //PromptSelectionResult psR = editor.SelectAll();
+
+            PromptSelectionResult psR = editor.GetSelection();
+            if (psR.Status == PromptStatus.OK)
+            {
+                ObjectId[] ids = psR.Value.GetObjectIds();
+                ChangeColors(ids);
+            }
+        }
+
+
+        private void ChangeColors(ObjectId[] ids)
+        {
+            Database database = HostApplicationServices.WorkingDatabase;
+            using (Transaction trans = database.TransactionManager.StartTransaction())
+            {
+                foreach (var id in ids)
+                {
+                    Entity entity = (Entity)id.GetObject(OpenMode.ForWrite);
+                    entity.ColorIndex = 3;
+                }
+                trans.Commit();
+            }
+        }
+
+
+        [CommandMethod("SelectDemo1")]
+        public void SelectDemo1()
+        {
+            Editor editor = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            //DxfCode 为枚举 
+            TypedValue[] typeValues = new TypedValue[] {
+                new TypedValue((int)DxfCode.Start,"Circle")
+            };
+            SelectionFilter filter = new SelectionFilter(typeValues);
+            PromptSelectionResult psR = editor.GetSelection(filter);
+            if (psR.Status == PromptStatus.OK)
+            {
+                ObjectId[] ids = psR.Value.GetObjectIds();
+                ChangeColors(ids);
+            }
+
+        }
+
+
+
+    }
+}
